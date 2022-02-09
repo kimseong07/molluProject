3e167d67ea559f8e752e446a816f333ff2ce5a90
3soojung <68137693+kungsae@users.noreply.github.com>
2022-02-09 11:51:35 +0900

    플레이어 움직임 방식 및 잡버그 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -45,8 +45,10 @@ public class Player : MonoBehaviour
 	}
 	private void FixedUpdate()
 	{
-		bool groundCheck = Physics2D.BoxCast(transform.position + transform.up * (boxCollider.offset.y - 0.01f), boxCollider.size, nowAngle, transform.right, 0.1f, LayerMask.GetMask("Ground"));
-		bool groundCheck2 = directionRaycast(-transform.up);
+		bool groundCheck = Physics2D.BoxCast(transform.position + transform.up * (boxCollider.offset.y - 0.01f), boxCollider.size, transform.rotation.eulerAngles.z, transform.right, 0.1f, LayerMask.GetMask("Ground"));
+		bool groundCheckBack = directionRaycast(-transform.up, (transform.right * -0.2f ));
+		bool groundCheckFront = directionRaycast(-transform.up, (transform.right * 0.2f ));
+		bool groundCheckMiddle = directionRaycast(-transform.up, transform.up * (boxCollider.offset.y - 0.1f));
 		if (hpDown)
 		{
 			hp -= Time.deltaTime;
@@ -54,7 +56,7 @@ public class Player : MonoBehaviour
 		}
 		else
 		{
-			hp += Time.deltaTime;
+			hp += Time.deltaTime * maxHp * 0.1f;
 			hp = Mathf.Clamp(hp, 0, maxHp);
 		}
 
@@ -62,10 +64,10 @@ public class Player : MonoBehaviour
 		speed = (hp / maxHp) * 10;
 		speed = Mathf.Clamp(speed, 2, 10);
 
-		transform.position += transform.right * x * Time.deltaTime;
+		rigid.velocity = transform.right*x;
 		animator.SetBool("Move", x != 0);
 
-		if (!groundCheck || !groundCheck2)
+		if (!groundCheck || (!groundCheckBack && !groundCheckFront)|| !groundCheckMiddle)
 		{
 			rigid.velocity = -transform.up * 9.8f;
 		}
@@ -81,14 +83,21 @@ public class Player : MonoBehaviour
 			sprite.flipX = true;
 		}
 
-		hit = Physics2D.Raycast(transform.position, transform.right * dir, 0.6f, LayerMask.GetMask("Ground"));
+		hit = Physics2D.Raycast(transform.position, transform.right*0.4f * dir, 0.51f, LayerMask.GetMask("Ground"));
 		if (hit.collider != null && x != 0)
 		{
-			if (directionAngleRaycast(transform.right * dir, transform.up) != nowAngle)
+			if (Input.GetKey(KeyCode.Space))
 			{
-				nowAngle = directionAngleRaycast(transform.right * dir, transform.up * dir);
+
 			}
-			transform.rotation = Quaternion.Euler(0, 0, nowAngle);
+			else
+			{
+				if (directionAngleRaycast(transform.right * dir, -transform.up*0.45f) != nowAngle)
+				{
+					nowAngle = directionAngleRaycast(transform.right * dir, -transform.up*0.45f * dir);
+				}
+				transform.rotation = Quaternion.Euler(0, 0, nowAngle);
+			}
 		}
 	}
 	void Update()
@@ -96,13 +105,13 @@ public class Player : MonoBehaviour
 		x = Input.GetAxisRaw("Horizontal") * speed;
 
 
-		UIManager.Instance.flowHp(hp, maxHp);
-		GamaManager.Instance.DeadCheck(hp);
-		if(GamaManager.Instance.isGameOver == true)
-        {
-			Invoke("GamaSet", 1f);
-			hp = maxHp;
-        }
+		//UIManager.Instance.flowHp(hp, maxHp);
+		//GamaManager.Instance.DeadCheck(hp);
+		//if(GamaManager.Instance.isGameOver == true)
+  //      {
+		//	Invoke("GamaSet", 1f);
+		//	hp = maxHp;
+  //      }
 
 	}
 	private void OnTriggerEnter2D(Collider2D collision)
@@ -130,7 +139,7 @@ public class Player : MonoBehaviour
 	{
 		if (hit.collider != null)
 		{
-			RaycastHit2D hit2 = Physics2D.Raycast(transform.position - downDir * 0.1f, dir, 1f, LayerMask.GetMask("Ground"));
+			RaycastHit2D hit2 = Physics2D.Raycast(transform.position + downDir, dir, 1f, LayerMask.GetMask("Ground"));
 			if (hit2.collider != null)
 			{
 				float dirAngle = GetAngle(hit2.point, hit.point);
@@ -146,9 +155,9 @@ public class Player : MonoBehaviour
 			return nowAngle;
 		}
 	}
-	private bool directionRaycast(Vector2 dir)
+	private bool directionRaycast(Vector2 dir,Vector3 originPos)
 	{
-		return Physics2D.Raycast(transform.position + (transform.right * -0.1f * this.dir), dir, 0.51f, LayerMask.GetMask("Ground"));
+		return Physics2D.Raycast(transform.position + originPos, dir, 0.5f, LayerMask.GetMask("Ground"));
 	}
 
 	void GamaSet()
